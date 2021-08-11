c2da59c339c7367d222a6ad9d4cd4995de73caad
Abdullah Ali <markosberg123@gmail.com>
2021-08-11 21:56:16 +0200

    first commit

---
+++ resources/views/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layout')
+
+@section('content')
+
+<style>
+    .container {
+      max-width: 450px;
+    }
+    .push-top {
+      margin-top: 50px;
+    }
+</style>
+
+<div class="card push-top">
+  <div class="card-header">
+    Add User
+  </div>
+
+  <div class="card-body">
+      <form method="post" action="{{ route('students.store') }}">
+          <div class="form-group">
+              @csrf
+              <label for="name">Name</label>
+              <input type="text" class="form-control" name="name"/>
+          </div>
+          <div class="form-group">
+              <label for="email">Email</label>
+              <input type="email" class="form-control" name="email"/>
+          </div>
+          <div class="form-group">
+              <label for="phone">Phone</label>
+              <input type="tel" class="form-control" name="phone"/>
+          </div>
+          <div class="form-group">
+              <label for="password">Password</label>
+              <input type="text" class="form-control" name="password"/>
+          </div>
+          <button type="submit" class="btn btn-block btn-danger mt-2">Create User</button>
+      </form>
+  </div>
+</div>
+@endsection
